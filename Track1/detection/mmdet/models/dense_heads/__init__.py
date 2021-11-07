c8fd8079ffedb62ac2866471f22ee2432f0770fb
Yuming Du <dulucas24@gmail.com>
2021-11-07 19:26:41 +0100

    Update __init__.py

--- Track1/detection/mmdet/models/dense_heads/__init__.py
+++ Track1/detection/mmdet/models/dense_heads/__init__.py
@@ -34,7 +34,7 @@ from .yolact_head import YOLACTHead, YOLACTProtonet, YOLACTSegmHead
 from .yolo_head import YOLOV3Head
 from .yolof_head import YOLOFHead
 from .yolox_head import YOLOXHead
-from .uvo_rpn_head import UVORPNHead
+from .uvo_rpn_head import UVORPNHead, UVOStageCascadeRPNHead
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
@@ -46,5 +46,5 @@ __all__ = [
     'SABLRetinaHead', 'CentripetalHead', 'VFNetHead', 'StageCascadeRPNHead',
     'CascadeRPNHead', 'EmbeddingRPNHead', 'LDHead', 'CascadeRPNHead',
     'AutoAssignHead', 'DETRHead', 'YOLOFHead', 'DeformableDETRHead',
-    'CenterNetHead', 'YOLOXHead', 'UVORPNHead'
+    'CenterNetHead', 'YOLOXHead', 'UVORPNHead', 'UVOStageCascadeRPNHead'
 ]
